e30c521cd038d00206ca2e0c647c7a9d3dc2f104
lcalem <laura.calem@gmail.com>
2019-07-22 16:38:54 +0200

    changed mAP callback so it better logs the current known labels proportion

--- model/callbacks/metric_callbacks.py
+++ model/callbacks/metric_callbacks.py
@@ -6,12 +6,13 @@ from model import metrics
 
 
 class MAPCallback(Callback):
-    def __init__(self, x_val, y_val, exp_folder):
+    def __init__(self, x_val, y_val, exp_folder, prop):
         super(Callback, self).__init__()
 
         self.exp_folder = exp_folder
         self.x_val = x_val
         self.y_val = y_val
+        self.prop = prop
         self.map = metrics.MAP()
 
     def on_epoch_end(self, epoch, logs={}):
@@ -27,7 +28,7 @@ class MAPCallback(Callback):
         map_score = sum(ap_scores) / len(ap_scores)
 
         with open(os.path.join(self.exp_folder, 'map.csv'), 'a') as f_out:
-            line = '%d,%6f,' % (epoch, map_score) + ','.join([str(s) for s in ap_scores]) + '\n'
+            line = '%d,%d,%6f,' % (self.prop, epoch, map_score) + ','.join([str(s) for s in ap_scores]) + '\n'
             f_out.write(line)
 
         print("interval evaluation - epoch: {:d} - mAP score: {:.6f}".format(epoch, map_score))
