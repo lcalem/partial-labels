fd12f15b669f365bc2df8d8d02c6cb99f5153337
lcalem <laura.calem@gmail.com>
2019-07-26 12:02:49 +0200

    scheduler callback

---
+++ model/callbacks/scheduler.py
@@ -0,0 +1,14 @@
+from model.utils import log
+from model.utils.config import cfg
+
+
+def lr_scheduler(epoch, lr):
+
+    if epoch in cfg.CALLBACK.LR_TRIGGER:
+        newlr = cfg.CALLBACK.LR_FACTOR * lr
+        log.printcn(log.WARNING, 'lr_scheduler: lr %g -> %g @ %d' % (lr, newlr, epoch))
+    else:
+        newlr = lr
+        log.printcn(log.OKBLUE, 'lr_scheduler: lr %g @ %d' % (newlr, epoch))
+
+    return newlr
