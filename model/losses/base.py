d5352193c8ebcdbf9c68c66f2c905c8e55b8f446
lcalem <laura.calem@gmail.com>
2019-09-10 18:30:42 +0200

    tf 1.14 fix

--- model/losses/base.py
+++ model/losses/base.py
@@ -5,7 +5,7 @@ class BaseLoss(object):
     def __init__(self):
         pass
 
-    def __call__(self, y_true, y_pred):
+    def __call__(self, y_true, y_pred, **kwargs):
         return self.compute_loss(y_true, y_pred)
 
     def compute_loss(self, y_true, y_pred):
