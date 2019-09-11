4c742e5f19130217535c2fa3cfd1e25acc906568
Olivier Petit <olivier.petith@gmail.com>
2019-09-11 12:06:06 +0200

    randomly removing organs in the patients CT-scans for organ segmentation with partially annotated data

--- data/ircad_lps/preprocessing/create_dataset_from_vtk.py
+++ data/ircad_lps/preprocessing/create_dataset_from_vtk.py
@@ -2,14 +2,15 @@ import numpy as np
 import glob
 import os
 import sys
+import random
 
 from vtk_utils import read_vtk
 
-def create_dataset_from_vtk(data_dir):
-    """
-    Create a dataset from vtk images
 
-    {background : 0, liver : 1, pancreas : 2, stomach : 3}
+def preprocess_images_from_vtk(data_dir):
+    """
+    Create the images directory from vtk images
+
     """
     
     all_patient_paths = glob.glob(os.path.join(data_dir, 'volumes/*'))
@@ -17,57 +18,114 @@ def create_dataset_from_vtk(data_dir):
     for c, patient_path in enumerate(all_patient_paths):
         pid = int(patient_path.split('/')[-1])
 
-        # Loading volume image and annotations for the Liver Pancreas and Stomach
+        # Loading volume image
         image_path = os.path.join(patient_path, '{}_image.vtk'.format(pid))
-        liver_annot_path = os.path.join(patient_path, '{}_mask_T_Liver.vtk'.format(pid))
-        pancreas_annot_path = os.path.join(patient_path, '{}_mask_Pancreas.vtk'.format(pid))
-        stomach_annot_path = os.path.join(patient_path, '{}_mask_Stomach.vtk'.format(pid))
-
         image, _ = read_vtk(image_path)
-        liver_annot, _ = read_vtk(liver_annot_path)
-        pancreas_annot, _ = read_vtk(pancreas_annot_path)
-        stomach_annot, _ = read_vtk(stomach_annot_path)
 
         if image.shape[1] != 512 or image.shape[2] != 512:
             print('Wrong shape : ', image.shape)
             continue
-        
-        # Clip image values between [-1000, 2000]
-        image = np.clip(image, -1000, 2000)
+
+        # Preprocess
+        image = image.astype(np.float32)
+        image = np.clip(image, -1000, 2000) # Clip image values between [-1000, 2000]
         
         image_mean = np.mean(image)
         image_std = np.std(image)
 
         # Image normalization, zero mean and unit variance at a patient scale
-        image = (image - image_mean) / image_std
-
-        # Assemble annotations
-        complete_annotation = np.zeros_like(liver_annot, dtype=np.uint8)
-        complete_annotation[np.where(liver_annot == 1)] = 1
-        complete_annotation[np.where(pancreas_annot == 1)] = 2
-        complete_annotation[np.where(stomach_annot == 1)] = 3
+        #image = (image - image_mean) / image_std
+        image = 255 * (image - np.min(image)) / (np.max(image) - np.min(image))
+        image = image.astype(np.int16)
 
         # Save the slices
         nb_slices, height, width = image.shape
         patient_images_path = os.path.join(data_dir, 'images/{}'.format(pid))
-        patient_annotation_path = os.path.join(data_dir, 'annotations/{}'.format(pid))
         if not os.path.isdir(patient_images_path):
             os.makedirs(patient_images_path)
-        if not os.path.isdir(patient_annotation_path):
-            os.makedirs(patient_annotation_path)
 
         for i in range(nb_slices):
             slice_image_path = os.path.join(data_dir, 'images/{}/{}.npy'.format(pid, str(i).zfill(3)))
-            slice_annot_path = os.path.join(data_dir, 'annotations/{}/{}.npy'.format(pid, str(i).zfill(3)))
-            
-            np.save(slice_image_path, image[i,:,:])
-            np.save(slice_annot_path, complete_annotation[i,:,:])
+            np.save(slice_image_path, [image[i,:,:],]*3)
 
-        print('Creating dataset : {} / {} ({:.2f}%)'.format(c, len(all_patient_paths), c/len(all_patient_paths)*100), end='\r')
+        print('Preprocessing images : {} / {} ({:.2f}%)'.format(c, len(all_patient_paths), c/len(all_patient_paths)*100), end='\r')
+
+
+def preprocess_annotations_from_vtk(data_dir, keep_annotation_proportion=100):
+    """
+    Create a dataset from vtk images
+
+    keep_annotation_proportion, int which gives the percentage of annotation to keep
+
+    {background : 0, liver : 1, pancreas : 2, stomach : 3}
+    """
+    
+    all_patient_paths = glob.glob(os.path.join(data_dir, 'volumes/*'))
+
+    # Removing annotations 
+    keep_annotation = {}
+    for patient in all_patient_paths:
+        keep_annotation[patient] = [1, 1, 1]
+
+    for i in range(3):
+        patient_with_removed_organ = random.sample(all_patient_paths, int(len(all_patient_paths) * (1 - keep_annotation_proportion/100)))
+        for patient in patient_with_removed_organ:
+            keep_annotation[patient][i] = 0
+
+    for c, patient_path in enumerate(all_patient_paths):
+        pid = int(patient_path.split('/')[-1])
+
+        # Loading volume annotations for the Liver Pancreas and Stomach
+        liver_annot_path = os.path.join(patient_path, '{}_mask_T_Liver.vtk'.format(pid))
+        pancreas_annot_path = os.path.join(patient_path, '{}_mask_Pancreas.vtk'.format(pid))
+        stomach_annot_path = os.path.join(patient_path, '{}_mask_Stomach.vtk'.format(pid))
+
+        liver_annot, _ = read_vtk(liver_annot_path)
+        pancreas_annot, _ = read_vtk(pancreas_annot_path)
+        stomach_annot, _ = read_vtk(stomach_annot_path)
+
+        if liver_annot.shape[1] != 512 or liver_annot.shape[2] != 512:
+            print('Wrong shape : ', liver_annot.shape)
+            continue
+
+
+        # Assemble annotations
+        complete_annotation = np.zeros_like(liver_annot, dtype=np.uint8)
+        if keep_annotation[patient_path][0] == 1:
+            complete_annotation[np.where(liver_annot > 0)] = 1
+        if keep_annotation[patient_path][2] == 1:
+            complete_annotation[np.where(stomach_annot > 0)] = 3
+        if keep_annotation[patient_path][1] == 1:
+            complete_annotation[np.where(pancreas_annot > 0)] = 2
+
+        # Save the slices
+        nb_slices, height, width = liver_annot.shape
+        patient_annotation_path = os.path.join(data_dir, 'annotations/{}/{}'.format(keep_annotation_proportion, pid))
+        if not os.path.isdir(patient_annotation_path):
+            os.makedirs(patient_annotation_path)
+        missing_organs_path = os.path.join(data_dir, 'missing_organs/{}/{}'.format(keep_annotation_proportion, pid))
+        if not os.path.isdir(missing_organs_path):
+            os.makedirs(missing_organs_path)
+
+
+        for i in range(nb_slices):
+            slice_annot_path =  os.path.join(data_dir, 'annotations/{}/{}/{}.npy'.format(keep_annotation_proportion, pid, str(i).zfill(3)))
+            slice_missing_annotation_path = os.path.join(data_dir, 'missing_organs/{}/{}/{}.npy'.format(keep_annotation_proportion, pid, str(i).zfill(3)))
+            
+            np.save(slice_annot_path, complete_annotation[i,:,:])
+            np.save(slice_missing_annotation_path, keep_annotation[patient_path])
+            
+
+        print('Creating downsampled dataset ({}) : {} / {} ({:.2f}%)'.format(keep_annotation_proportion, c, len(all_patient_paths), c/len(all_patient_paths)*100), end='\r')
 
 
 # python3 create_dataset_from_vtk /path/to/data/dir
 if __name__ == "__main__":
+    # Seed for sampling reproductibility
+    random.seed(654654)
+    organ_proportions = (1, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
     print('Start dataset creation')
-    create_dataset_from_vtk(sys.argv[1])
+    preprocess_images_from_vtk(sys.argv[1])
+    for prop in organ_proportions:
+        preprocess_annotations_from_vtk(sys.argv[1], prop)
     print('Done.')
--- model/networks/seg_baseline.py
+++ model/networks/seg_baseline.py
@@ -9,7 +9,7 @@ keras_applications.set_keras_submodules(
 )
 
 from tensorflow.keras import Model, Input
-from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.applications import ResNet50, Xception
 
 from tensorflow.keras.layers import Dense, Flatten, GlobalAveragePooling2D
 
@@ -41,10 +41,41 @@ class DiceScore(tf.keras.metrics.Metric):
         self.true_sum.assign_add(tf.reduce_sum(y_true))
         self.pred_sum.assign_add(tf.reduce_sum(y_pred))
 
-
     def result(self):
         epsilon = tf.keras.backend.epsilon()
         return (2. * self.intersection_sum + epsilon) / (self.true_sum + self.pred_sum + epsilon)
+
+
+class WeightedCrossEntropy(tf.keras.losses.Loss):
+    def __init__(self, class_weights):
+        super().__init__()
+        self.name = 'weighted_cross_entropy'
+        self.class_weights = tf.constant(class_weights, dtype=tf.float32, name='class_weights')
+    
+    def call(self, y_true, y_pred):
+        y_true = tf.cast(y_true, dtype=tf.float32)
+        y_pred = tf.cast(y_pred, dtype=tf.float32)
+        
+        weights = tf.reduce_sum(self.class_weights * y_true, axis=-1)
+        unweighted_cross_entropy = tf.keras.losses.categorical_crossentropy(y_true, y_pred)
+        cross_entropy = tf.reduce_mean(unweighted_cross_entropy * weights)
+        return cross_entropy
+
+
+class WeightedCrossEntropy_v2(tf.keras.losses.Loss):
+    def __init__(self, class_weights):
+        super().__init__()
+        self.name = 'weighted_cross_entropy'
+        self.class_weights = tf.constant(class_weights, shape=(4,), dtype=tf.float32, name='class_weights')
+    
+    def call(self, y_true, y_pred):
+        y_true = tf.reshape(y_true, shape=(-1, 4))
+        y_pred = tf.reshape(y_pred, shape=(-1, 4))
+    
+        weights = tf.reduce_sum(self.class_weights * y_true, axis=-1)
+        unweighted_cross_entropy = tf.keras.losses.categorical_crossentropy(y_true, y_pred)
+        cross_entropy = tf.reduce_mean(unweighted_cross_entropy * weights)
+        return cross_entropy
 
 
 
@@ -62,12 +93,15 @@ class SegBaseline(BaseModel):
 
     def build(self):
 
-        resnet = ResNet50(include_top=False, weights=None, input_shape=self.input_shape)
-
+        resnet = ResNet50(include_top=False, weights='imagenet', input_shape=self.input_shape)
+        
         x = resnet.output
         x = tf.keras.layers.Conv2D(self.n_classes, 1, activation=None)(x)
-        x = tf.keras.layers.Lambda(lambda x: tf.image.resize_bilinear(x, [self.input_shape[0], self.input_shape[1]]))(x)
-
+        x = tf.keras.layers.UpSampling2D(size=(32,32), interpolation='bilinear')(x)
+        #proba = tf.keras.layers.Activation('relu')(x)
+        #x = tf.keras.layers.Conv2D(self.n_classes, 3, activation=None, padding='same')(x)
+        #x = tf.keras.layers.Lambda(lambda x: tf.image.resize_bilinear(x, [self.input_shape[0], self.input_shape[1]]))(x)
+        
         proba = tf.keras.layers.Activation('softmax')(x)
         
         self.model = Model(inputs=resnet.inputs, outputs=proba, name='cls_model')
@@ -75,8 +109,19 @@ class SegBaseline(BaseModel):
         self.log('Outputs shape %s' % str(self.model.output_shape))
 
         optimizer = self.build_optimizer()
-
-        self.model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['categorical_accuracy', DiceScore(class_id=1)])
+        
+        metrics = ['categorical_accuracy',
+                   DiceScore(class_id=1, name='liver_dice'),
+                   DiceScore(class_id=2, name='pancreas_dice'),
+                   DiceScore(class_id=3, name='stomach_dice')]
+        
+        #loss = WeightedCrossEntropy([1.0, 3.0, 50.0, 30.0])
+        #loss = WeightedCrossEntropy_v2([1.011276563, 144.6087607, 1090.794364, 301.3094671])
+        loss = WeightedCrossEntropy_v2([0.04292439486, 0.6141659478, 5.935304885, 2.689794809])
+        #loss = WeightedCrossEntropy([0.004869942426, 2.160194604, 3.037742885, 2.479012777])
+        #loss = tf.keras.losses.CategoricalCrossentropy()
+        
+        self.model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
 
         if self.verbose:
             self.log('Final model summary')
