f3cecab68c86bf8e3fded4809b3780ec50cc0f3d
Olivier Petit <olivier.petith@gmail.com>
2019-10-04 18:38:48 +0200

    moving segmentation relabeling to the SegmentationRelabeling class

--- model/relabel/__init__.py
+++ model/relabel/__init__.py
@@ -2,3 +2,4 @@ from model.relabel.relabel_prior import PriorRelabeling
 from model.relabel.relabel_sk import SkRelabeling
 from model.relabel.relabel_all import AllSkRelabeling
 from model.relabel.relabel_baseline import BaselineRelabeling
+from model.relabel.relabel_segmentation import SegmentationRelabeling
