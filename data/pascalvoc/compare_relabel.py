a64aa88a6e7f5f924e659bf909bd96187f155cb5
lcalem <laura.calem@gmail.com>
2019-10-08 15:30:07 +0200

    changed relabel comparison script to include true positive / truc negative etc metrics

--- data/pascalvoc/compare_relabel.py
+++ data/pascalvoc/compare_relabel.py
@@ -33,29 +33,45 @@ def compare_relabel_dataset(dataset_original, dataset_relabel):
     relabel_data = read_dataset(dataset_relabel)
 
     count_relabeled = 0
-    count_correct = 0
     count_zeros = 0
+
+    true_positives = 0
+    true_negatives = 0
+    false_positives = 0
+    false_negatives = 0
 
     # for each unknown label in the original dataset, check if it has been relabeled and if the relabel is correct
     for img_id, img_labels in ori_data.items():
 
         for i, val in enumerate(img_labels):
+            # we only consider the unknown labels ofc
             if val == 0:
                 count_zeros += 1
                 if relabel_data[img_id][i] != 0:
                     count_relabeled += 1
-                    if relabel_data[img_id][i] == gt_data[img_id][i]:
-                        count_correct += 1
+
+                    if relabel_data[img_id][i] == 1:
+                        if gt_data[img_id][i] == 1:
+                            true_positives += 1
+                        elif gt_data[img_id][i] == -1:
+                            false_positives += 1
+
+                    elif relabel_data[img_id][i] == -1:
+                        if gt_data[img_id][i] == -1:
+                            true_negatives += 1
+                        elif gt_data[img_id][i] == 1:
+                            false_negatives += 1
             else:
                 # check we didn't relabel data that wasn't zero
                 assert gt_data[img_id][i] == val
                 assert relabel_data[img_id][i] == val
 
+    count_correct = true_positives + true_negatives
     count_incorrect = count_relabeled - count_correct
     print('Out of %s zeros in the original dataset, %s (%2f %%) have been relabeled, %s (%2f %%) correct, %s (%2f %%) incorrect' % (count_zeros, count_relabeled, count_relabeled * 100 / count_zeros, count_correct, count_correct * 100 / count_relabeled, count_incorrect, count_incorrect * 100 / count_relabeled))
-
+    print('TP: %s\nFP: %s\nTN: %s\nFN: %s' % (true_positives, false_positives, true_negatives, false_negatives))
 
 # python3 compare_relabel.py /home/caleml/datasets/pascalvoc/VOCdevkit/VOC2007/Annotations/annotations_multilabel_trainval_partial_50_1.csv /home/caleml/partial_experiments/exp_20190911_1812_baseline/relabeling/relabeling_0_50p.csv
-# python3 compare_relabel.py /home/caleml/datasets/pascalvoc/VOCdevkit/VOC2007/Annotations/annotations_multilabel_trainval_partial_50_1.csv /home/caleml/partial_experiments/exp_20190918_1535_baseline/relabeling/relabeling_0_50p.csv
+# python3 compare_relabel.py /home/caleml/datasets/pascalvoc/VOCdevkit/VOC2007/Annotations/annotations_multilabel_trainval_partial_50_1.csv /home/caleml/partial_experiments/exp_20191007_1907_baseline_logits/relabeling/relabeling_0_50p.csv
 if __name__ == '__main__':
     compare_relabel_dataset(sys.argv[1], sys.argv[2])
