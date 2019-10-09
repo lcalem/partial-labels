96da697bbc5fbbdbae8f4c54bd389300ee0977a8
lcalem <laura.calem@gmail.com>
2019-10-09 16:40:28 +0200

    Reworked sk relabeling into visual relabeling with more control on the relabeled values selection

--- model/relabel/__init__.py
+++ model/relabel/__init__.py
@@ -1,5 +1,5 @@
 from model.relabel.relabel_prior import PriorRelabeling
-from model.relabel.relabel_sk import SkRelabeling
+from model.relabel.relabel_visual import VisualRelabeling
 from model.relabel.relabel_all import AllSkRelabeling
 from model.relabel.relabel_baseline import BaselineRelabeling
 from model.relabel.relabel_segmentation import SegmentationRelabeling
