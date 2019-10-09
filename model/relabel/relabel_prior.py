8b1baed7d39ff5ac7f8b48d7a745902b803405c7
lcalem <laura.calem@gmail.com>
2019-10-09 16:39:24 +0200

    reworked prior relabeling to add the yk selection via config-controlled proportions for positives and negatives

--- model/relabel/relabel_prior.py
+++ model/relabel/relabel_prior.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 
 from model import priors
-from model.relabel.base import Relabelator
+from model.relabel.base import ClassifRelabelator
 from model.utils import log
 
 from config.config import cfg
@@ -11,23 +11,26 @@ from config.config import cfg
 from pprint import pprint
 
 
-class PriorRelabeling(Relabelator):
+class PriorRelabeling(ClassifRelabelator):
 
-    def __init__(self, exp_folder, p, nb_classes, selection_type='proportion_ones', threshold=0.33):
+    def __init__(self, exp_folder, p, nb_classes, selection_params):
         '''
         Selection types:
-            - proportion_ones: selects _threshold_ percent of the y_k values that match sk > 0.5 and put them as ones
-            - proportion: selects _threshold_ / 2 percent of the y_k values and put them as -1 or +1
+            - proportion_ones:
+                - selects _threshold_ percent of the y_f values that match sk > 0.5 and put them as ones
+            - proportion_yf:
+                - selection from yf values (prop_ones and prop_zeros)
+                - relabel value depends on the chunk of yf selected (highest values -> +1, lowest -> -1)
         '''
         self.exp_folder = exp_folder
         self.p = p
         self.nb_classes = nb_classes
         self.prior = self.load_prior(cfg.RELABEL.PRIOR)
 
-        assert selection_type in ['proportion_ones', 'proportion']
-        assert 0.0 <= threshold <= 1.0
-        self.selection_type = selection_type
-        self.threshold = threshold
+        selection_params = {k.lower(): v for k, v in selection_params.items()}
+        self.selection_type = selection_params['type']
+        assert self.selection_type in ['proportion_ones', 'proportion_yf']
+        self.selection_params = selection_params
 
     def load_prior(self, name):
         if name == 'conditional':
@@ -42,13 +45,13 @@ class PriorRelabeling(Relabelator):
 
         Steps for relabeling:
         - compute the relevant prior given the ground truth
-        - combine the prior with the predictions (-> y_k)
-        - Use this y_k and the original predictions y_pred to picj the relabeled examples
+        - combine the prior with the predictions (-> y_f)
+        - Use this y_f and the original predictions y_pred to picj the relabeled examples
 
-        yk: prior-weighted outputs
+        yf: prior-weighted outputs
         y_true: true batch (with zeros where the label is missing)
 
-        1. find the values of yk for which there is a missing label at the same index in the y_true batch
+        1. find the values of yf for which there is a missing label at the same index in the y_true batch
         2. order those values and get the corresponding indexes
         '''
 
@@ -57,12 +60,12 @@ class PriorRelabeling(Relabelator):
 
         # print('shape of y_pred %s' % str(y_pred.shape))
         p_k = self.prior.compute_pk(y_true)
-        y_k = self.prior.combine(y_pred, p_k)
+        y_f = self.prior.combine(y_pred, p_k)
 
         # relabeling, nb_added = self.prior.pick_relabel(y_pred, y_k, y_batch[0])  # (BS, K)
 
         assert y_pred.shape == (cfg.BATCH_SIZE, self.nb_classes),   'wrong y_pred shape %s' % str(y_pred.shape)
-        assert y_k.shape == (cfg.BATCH_SIZE, self.nb_classes),      'wrong yk shape %s' % str(y_k.shape)
+        assert y_f.shape == (cfg.BATCH_SIZE, self.nb_classes),      'wrong yk shape %s' % str(y_k.shape)
         assert y_true.shape == (cfg.BATCH_SIZE, self.nb_classes),   'wrong y_true shape %s' % str(y_true.shape)
 
         relabel_batch = np.copy(y_true)
@@ -72,12 +75,12 @@ class PriorRelabeling(Relabelator):
         # find and sort the relevant values of the outputs
         if self.selection_type == 'proportion_ones':
 
-            relevant_yk = np.where((y_true == 0) & (y_pred > 0.5), y_k, 0)   # consider only the values for missing labels and for which the initial prediction is > 0.5
-            sorted_indexes = np.argsort(relevant_yk, axis=None)   # the indexes are flattened
-            sorted_values = [relevant_yk[i // self.nb_classes][i % self.nb_classes] for i in sorted_indexes]
+            relevant_yf = np.where((y_true == 0) & (y_pred > 0.5), y_f, 0)   # consider only the values for missing labels and for which the initial prediction is > 0.5
+            sorted_indexes = np.argsort(relevant_yf, axis=None)   # the indexes are flattened
+            sorted_values = [relevant_yf[i // self.nb_classes][i % self.nb_classes] for i in sorted_indexes]
 
             # take the best values and find corresponding indexes
-            nb_ok_indexes = int((np.count_nonzero(sorted_values)) * self.threshold)
+            nb_ok_indexes = int((np.count_nonzero(sorted_values)) * self.selection_params['threshold'])
             final_indexes = [(i // self.nb_classes, i % self.nb_classes) for i in sorted_indexes[len(sorted_values) - nb_ok_indexes: len(sorted_values)]]
 
             # put the selected values as 1 in the relabel batch
@@ -87,14 +90,14 @@ class PriorRelabeling(Relabelator):
 
             nb_added_pos = nb_ok_indexes
 
-        elif self.selection_type == 'proportion':
-            relevant_yk = np.where(y_true == 0, y_k, 0)   # consider only the values for missing labels
-            sorted_indexes = np.argsort(relevant_yk, axis=None)   # the indexes are flattened
-            sorted_values = [relevant_yk[i // self.nb_classes][i % self.nb_classes] for i in sorted_indexes]
+        elif self.selection_type == 'proportion_yf':
+            relevant_yf = np.where(y_true == 0, y_f, 0)   # consider only the values for missing labels
+            sorted_indexes = np.argsort(relevant_yf, axis=None)   # the indexes are flattened
+            sorted_values = [relevant_yf[i // self.nb_classes][i % self.nb_classes] for i in sorted_indexes]
 
             # highest values -> +1
-            nb_positive_indexes = int((np.count_nonzero(sorted_values)) * (self.threshold / 2))
-            positive_indexes = [(i // self.nb_classes, i % self.nb_classes) for i in sorted_indexes[len(sorted_values)-nb_positive_indexes: len(sorted_values)]]
+            nb_positive_indexes = int((np.count_nonzero(sorted_values)) * self.selection_params['prop_pos'])
+            positive_indexes = [(i // self.nb_classes, i % self.nb_classes) for i in sorted_indexes[len(sorted_values) - nb_positive_indexes: len(sorted_values)]]
 
             # put the selected values as 1 in the relabel batch
             xs_pos = [elt[0] for elt in positive_indexes]
@@ -103,7 +106,7 @@ class PriorRelabeling(Relabelator):
 
             # lowest values -> -1
             nb_zeros = len(sorted_values) - np.count_nonzero(sorted_values)
-            nb_negative_indexes = int((np.count_nonzero(sorted_values)) * (self.threshold / 2))
+            nb_negative_indexes = int((np.count_nonzero(sorted_values)) * self.selection_params['prop_neg'])
             negative_indexes = [(i // self.nb_classes, i % self.nb_classes) for i in sorted_indexes[nb_zeros: nb_zeros + nb_negative_indexes]]
 
             xs_neg = [elt[0] for elt in negative_indexes]
@@ -155,34 +158,3 @@ class PriorRelabeling(Relabelator):
                 self.f_relabel.write(relabel_line)
 
             self.seen_keys.add(img_id)
-
-    def init_step(self, relabel_step_nb):
-        # save new targets as file
-        self.targets_path = os.path.join(self.exp_folder, 'relabeling', 'relabeling_%s_%sp.csv' % (relabel_step_nb, self.p))
-        os.makedirs(os.path.dirname(self.targets_path), exist_ok=True)
-
-        self.total_added = 0
-        self.positive_added = 0
-        self.negative_added = 0
-
-        self.seen_keys = set()
-        self.relabel_step = relabel_step_nb
-
-        self.f_relabel = open(self.targets_path, 'w+')
-
-    def finish_step(self, relabel_step):
-        assert relabel_step == self.relabel_step
-
-        relabel_logpath = os.path.join(self.exp_folder, 'relabeling', 'log_relabeling.csv')
-        with open(relabel_logpath, 'a') as f_log:
-            f_log.write('%s,%s,%s,%s,%s\n' % (self.p, self.relabel_step, self.total_added, self.positive_added, self.negative_added))
-
-        log.printcn(log.OKBLUE, '\tAdded %s labels during relabeling, logging into %s' % (self.total_added, relabel_logpath))
-        log.printcn(log.OKBLUE, '\tNew dataset path %s' % (self.targets_path))
-
-        self.f_relabel.close()
-
-    def __del__(self):
-        if hasattr(self, 'f_relabel'):
-            self.f_relabel.close()
-
