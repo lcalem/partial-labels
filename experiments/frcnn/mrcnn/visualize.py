c402c7d35f02d3c006cc023a9dca70983dd9a8d8
lcalem <laura.calem@gmail.com>
2019-12-12 15:41:49 +0100

    fixing class names

--- experiments/frcnn/mrcnn/visualize.py
+++ experiments/frcnn/mrcnn/visualize.py
@@ -145,7 +145,7 @@ def display_instances(image,
         if not captions:
             class_id = class_ids[i]
             score = scores[i] if scores is not None else None
-            label = class_names[class_id]
+            label = class_names[class_id + 1]  # there is no background now
             caption = "{} {:.3f}".format(label, score) if score else label
         else:
             caption = captions[i]
